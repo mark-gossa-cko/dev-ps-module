7d6d1cfc4ffc53a1dff5ec70ab879cbfed19359f
Mark Gossa <mark.gossa@checkout.com>
2025-10-20 15:30:12 +0100

    Adds codeartifict login to cal cmdlet

--- src/DevPsModule/DevPsModule.psm1
+++ src/DevPsModule/DevPsModule.psm1
@@ -26,8 +26,9 @@ function cal {
     $aws_okta_app="0oa423kknpZCS07GJ357"
     $role_name="cko_issuing_engineer"
     $account_id="791259062566"
-    okta-aws-cli --org-domain $domain --oidc-client-id $okta_app --aws-acct-fed-app-id $aws_okta_app -b -z -r arn:aws:iam::$($account_id):role/$role_name -i arn:aws:iam::$($account_id):saml-provider/okta -s 43200
     
+    okta-aws-cli --org-domain $domain --oidc-client-id $okta_app --aws-acct-fed-app-id $aws_okta_app -b -z -r arn:aws:iam::$($account_id):role/$role_name -i arn:aws:iam::$($account_id):saml-provider/okta -s 43200
+    aws codeartifact login --tool dotnet --repository cko-packages --domain cko-packages --region eu-west-1
     Dotnet restore $Path
 }
 
